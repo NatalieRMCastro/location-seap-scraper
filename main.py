56bab672ade6646392da85d7cb9b0582b245e387
Natalie Castro <134972160+NatalieRMCastro@users.noreply.github.com>
2025-10-27 19:52:48 -0400

    Update main.py
    adding the canonical location based changes, and testing with three small places

--- main.py
+++ main.py
@@ -5,39 +5,47 @@ from time import sleep
 
 
 def main():
-    queries = ['jobs near me', 'careers in', 'jobs hiring']
-    daily_pulls = []
-    daily_suggests = []
+    #queries = ['jobs near me', 'careers in', 'jobs hiring']
+    location_pulls = []
+    location_suggests = []
+
+    city_list = pd.read_csv("USA City Locs.csv")
+    loc_query_tails = city_list['City State Query'].to_list()
+
+    ## Adding in a few test queries just to test a smaller sample
+    loc_query_tails = ['Alexander City Alabama', 'Tucson Arizona', 'Litchfield Michigan']
 
        
     ## Iterating through each seed query, assigning a variable to the seed
-    for seed in queries:
+    for seed in loc_query_tails:
+        base_query = "jobs in "
+        query = base_query + seed.lower()
         ## generating the suggests item
-        s = suggests.get_suggests(seed, source='google')
+        s = suggests.get_suggests(query, source='google')
         suggestion = {seed:s['suggests']}
-        daily_suggests.append(suggestion)
+        location_suggests.append(suggestion)
         ## generating the tree from the suggests item
-        tree = suggests.get_suggests_tree(seed,source='google')
+        tree = suggests.get_suggests_tree(query,source='google')
 
         ## generating edges from the tree
         edges = suggests.to_edgelist(tree)
 
         ## generating the parents from the edges
         parents = suggests.add_parent_nodes(edges)
+        location_pulls.append(grandparents
 
         ## generating the grandparents from the parents
         grandparents = parents.apply(suggests.add_metanodes,axis=1)
         ## appending the dataframe returned to a larger list of dataframes
-        daily_pulls.append(grandparents)
+        location_pulls.append(grandparents)
         ## sleeping
-        sleep(3)
+        sleep(1.5)
 
-    df = pd.concat(daily_pulls)
+    df = pd.concat(location_pulls)
 
-    curr_date = strftime("%Y-%m-%d", gmtime())
-    print (curr_date)
+    print (seed)
     
-    df.to_csv(f'{curr_date}-autocomplete.csv')
+    df.to_csv(f'{seed}-autocomplete.csv')
 
 
 if __name__=="__main__":

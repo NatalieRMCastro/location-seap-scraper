4c2902bf8ec9a5b9a448013c557a5312a0ae3873
Natalie Castro <134972160+NatalieRMCastro@users.noreply.github.com>
2025-10-28 10:10:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-   'pandas','suggests','time'
+   'pandas','suggests'
 ]
 
 # The rest you shouldn't have to touch too much :)
